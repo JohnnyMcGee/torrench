06038511cb795f1d34ffa5cd348a981a188aa3b7
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-05-26 08:26:04 -0500

    Remove extra lines & semicolons

--- data/torrench.py
+++ data/torrench.py
@@ -11,7 +11,7 @@ the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 Torrench is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
+but WITHOUT ANY WARRANTY without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
@@ -24,47 +24,40 @@ import sys
 import argparse
 import urllib
 from TorrentDetail import TorrentDetail
-try:
-	import requests
-	from bs4 import BeautifulSoup
-	from tabulate import tabulate
-	from termcolor import colored
-	import find_url
-except ImportError as e:
-	print(e);
-	print("Please install and retry.\n")
-	sys.exit()
+import requests
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+from termcolor import colored
+import find_url
 
 def init(args):
-	# Resolve arguments - BEGIN
-	input_title = ' '.join(args.search)
-	print(input_title)
+	title = urllib.parse.quote(' '.join(args.search))
 	page_limit = args.limit
 	if args.clear_html:
 		home = os.path.expanduser('~')
 		temp_dir = home+"/.torrench/temp/"
-		files = os.listdir(temp_dir);
+		files = os.listdir(temp_dir)
 		if not files:
 			print("\nNothing to remove\n")
 		else:
 			count=0
 			for i in files:
-				os.remove(os.path.join(temp_dir, i));
-				count = count+1;
+				os.remove(os.path.join(temp_dir, i))
+				count = count+1
 			print("\nRemoved %d file(s).\n" %count)
 		sys.exit()
-	if len(input_title) == 0:
-		print("\nSearch string expected.\nUse --help for more\n");
-		sys.exit();
+	if len(title) == 0:
+		print("\nSearch string expected.\nUse --help for more\n")
+		sys.exit()
 	elif page_limit <= 0 or page_limit > 50:
 		print("Enter valid page input [0<p<=50]")
-		sys.exit();
+		sys.exit()
 	elif not os.path.isdir(args.directory):
 		print("Invalid directory. Please try again")
 		sys.exit()
 	else:
 		main(
-			input_title,
+			title,
 			page_limit,
 			directory=args.directory,
 			rtorrent=args.rtorrent,
@@ -72,22 +65,21 @@ def init(args):
 			magnet=args.magnet,
 			info=args.info,
 			category=args.category)
-	# Resolve input arguments - END
 
 def main(
-    input_title, page_limit,
+    title: str,
+    page_limit: int,
 	category: int=0,
     html: bool=False,
     magnet: bool=False,
     info: bool=False,
 	rtorrent: bool=False,
 	directory: str="./"
-    ):
+    ) -> None:
 	# Get proxy list
 	url_list = []
 	url_list = find_url.find_url_list()
 	
-	title = urllib.parse.quote(input_title)
 	total_result_count = 0
 	page_result_count = 9999
 	details_link = {}
@@ -99,13 +91,13 @@ def main(
 		for p in range(page_limit):
 			# If results in a page are <30, break loop (no more remaining pages are fetched)
 			if page_result_count < 30:
-				break;
+				break
 			
 			if page_limit > 1:
 				fetch_status_str = "\nFetching from page: "+str(p+1)
 			else:
 				fetch_status_str = "\n(Optional) Use [-p] option to specify pages.\n\nFetching results (Max: 30)...\n(Might take longer. Be patient)" 
-			print(fetch_status_str);
+			print(fetch_status_str)
 			
 			page_result_count = 0
 			# Determine proxy site to use - list to proxy sites is obtained from find_url module
@@ -116,9 +108,9 @@ def main(
 				try:
 					raw = requests.get(url+"/search/"+title+"/1/99/"+str(category))
 					raw = raw.content
-					break;
+					break
 				except requests.exceptions.ConnectionError as e:
-					print("Link Not reachable... Trying next proxy...");
+					print("Link Not reachable... Trying next proxy...")
 					url_list_count += 1
 					url = url_list[url_list_count]
 			# End determining proxy site
@@ -128,7 +120,7 @@ def main(
 				content = soup.find_all('table', id="searchResult")[0]
 			except IndexError:
 				if p == 0:
-					print("\nNo results found for given input!");
+					print("\nNo results found for given input!")
 					break	
 					
 			data = content.find_all('tr')
@@ -143,7 +135,7 @@ def main(
 				else:
 					comment = "0"
 				if name == None or uploader == None:
-					continue;
+					continue
 				name = name.string
 				uploader = uploader.string 
 				total_result_count+=1		
@@ -162,7 +154,7 @@ def main(
 				leeches = i.find_all('td', align="right")[1].string
 				date = i.find('font', class_="detDesc").get_text().split(' ')[1].replace(',', "")
 				size = i.find('font', class_="detDesc").get_text().split(' ')[3].replace(',', "")
-				torr_id = i.find('a', {'class': 'detLink'})["href"].split('/torrent/')[1];
+				torr_id = i.find('a', {'class': 'detLink'})["href"].split('/torrent/')[1]
 				link = url+"/torrent/"+torr_id		
 				### Extraction ends here ###
 				
@@ -174,45 +166,45 @@ def main(
 				# Dictionary to map torrent name with corresponding link (Used later)
 				details_link[str(total_result_count)] = link
 				details_name[str(total_result_count)] = name
-			print(">> "+str(page_result_count)+" torrents");
+			print(">> "+str(page_result_count)+" torrents")
 	except KeyboardInterrupt:
 		print("\nAborted!\n")
 	
     # Print Results and fetch torrent details
 	if(total_result_count > 0):
-		print("\n\nS=Seeds; L=Leeches; C=Comments");
+		print("\n\nS=Seeds L=Leeches C=Comments")
 		final_output = tabulate(masterlist, headers=['TYPE', 'NAME', 'INDEX', 'UPLOADER', 'SIZE','S','L', 'UPLOADED', "C"], tablefmt="grid")
-		print(final_output);
-		print("\nTotal: "+str(total_result_count)+" torrents");
+		print(final_output)
+		print("\nTotal: "+str(total_result_count)+" torrents")
 		exact_no_of_pages = total_result_count//30
 		has_extra_page = total_result_count%30
 		if has_extra_page > 0:
 			exact_no_of_pages +=1			
-		print("Total pages: "+str(exact_no_of_pages));
-		print("\nFurther, a torrent's details can be fetched (Description, comments, download(Magnetic) Link, etc.)");
+		print("Total pages: "+str(exact_no_of_pages))
+		print("\nFurther, a torrent's details can be fetched (Description, comments, download(Magnetic) Link, etc.)")
 		
 		# Fetch torrent details
 		import details
-		print("Enter torrent's index value to fetch details (Maximum one index)\n");
+		print("Enter torrent's index value to fetch details (Maximum one index)\n")
 		option = 9999
   
   
 		while(option != 0):
 			try:
-				option = int(input("(0 = exit)\nindex > "));
+				option = int(input("(0 = exit)\nindex > "))
 				if option > total_result_count or option < 0 or option == "":
-					print("**Enter valid index**\n\n");
-					continue;
+					print("**Enter valid index**\n\n")
+					continue
 				elif option == 0:
-					break;
+					break
 				else:
 					selected_link = details_link[str(option)]
 					selected_name = details_name[str(option)]
 					t = TorrentDetail(selected_link, selected_name)
 					if html:
-						print("Fetching details for torrent index [%d] : %s" %(option, selected_name));
+						print("Fetching details for torrent index [%d] : %s" %(option, selected_name))
 						file_url = details.save_as_html(t)
-						print("\nFile URL: "+file_url+"\n\n"); 
+						print("\nFile URL: "+file_url+"\n\n") 
 					if info:
 						d = details.get_info(selected_link, str(option))
 						print(d)
@@ -226,10 +218,10 @@ def main(
 						print(t.nfo)
 
 			except KeyboardInterrupt:
-				break;
+				break
 			except ValueError:
-				print("Check input! (Enter one (integer) index at a time)\n\n");
-		print("\nDone");
+				print("Check input! (Enter one (integer) index at a time)\n\n")
+		print("\nDone")
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="A simple torrent search tool.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -244,5 +236,5 @@ if __name__ == "__main__":
 	parser.add_argument("--info", action="store_true", default=False, help="print info about the torrent")
 	parser.add_argument("--html", action="store_true", default=False, help="save details to html")
 	args = parser.parse_args()
-	init(args);
+	init(args)
 	
